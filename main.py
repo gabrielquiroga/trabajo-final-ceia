c8882ff48840af173b6963e97a7c38563d30122c
Gabriel <quiroga.m.gabriel@gmail.com>
2026-04-05 11:12:10 -0300

    Created trajectory generation poc with saving to HDF5

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from trabajo-final-ceia!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from trabajo-final-ceia!")
+
+
+if __name__ == "__main__":
+    main()
